62482ee9c372db4a3dbe32b1bb36245688f39a39
BabaDee94 <adedayoadenle27@gmail.com>
2022-09-13 16:40:45 -0700

    love tech

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,43 @@
 #include "main.h"
 
+
+
 /**
- * main - Entry point
  *
- * Description: Writing a program to print _putchar with 0 as return
- * Return: Alwasys 0 (Success)
- */
+ *  * main - print putchar
+ *
+ *   * Return: always 0
+ *
+ *    */
+
+
+
 int main(void)
+
 {
-char c[] = "_putchar";
-int i;
 
-for  (i = 0; i < 8; i++)
-{
-_putchar(c[i]);
-}
+		_putchar('_');
 
-_putchar('\n');
+			_putchar('p');
 
-return (0);
+				_putchar('u');
+
+					_putchar('t');
+
+						_putchar('c');
+
+							_putchar('h');
+
+								_putchar('a');
+
+									_putchar('r');
+
+										_putchar('\n');
+
+
+
+											return (0);
+
+
+
 }
