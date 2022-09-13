fabb6e38c9865fd6ec8d63664f20a5c02feb88c6
BabaDee94 <adedayoadenle27@gmail.com>
2022-09-13 15:54:15 -0700

    love tech

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main- check the code
+ *
+ * Return: Always 0 (Success)
+ * Description: Am beginning to understand the codes
+ */
+int main(void)
+{
+Print_alphabet();
+return (0);
+}
