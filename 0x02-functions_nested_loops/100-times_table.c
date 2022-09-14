d173bfdfcfc0490babbfa990188b4fb9df778cd0
BabaDee94 <adedayoadenle27@gmail.com>
2022-09-14 01:30:16 -0700

    love tech

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * print_times_table - Prints a multiplication table up to param
  * @n: The number to be treated
@@ -18,43 +16,39 @@ if (n >= 0 && n <= 14)
 for (x = 0; x <= n; x++)
 
 {
-
 for (y = 0; y <= n; y++)
-
+}
+z = x * y;
+if (z > 99)
 {
-																			z = x * y;
-																			if (z > 99)
-																			{
-																			_putchar(',');
-																			_putchar(32);
-																			_putchar((z / 100) + '0');
-																			_putchar(((z / 10) % 10) + '0');																																	
-																			_putchar((z % 10) + '0');
+_putchar(',');
+_putchar(32);
+_putchar((z / 100) + '0');
+_putchar(((z / 10) % 10) + '0');													
+_putchar((z % 10) + '0');
+}
+else if (z > 9)	
+{
+_putchar(',');
+_putchar(32)																		
+_putchar(32);
+_putchar(((z / 10) % 10) + '0');
+_putchar((z % 10) + '0');
+}
+else
+{
+if (y != 0)
+{
+_putchar(',');
+_putchar(32);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
+}																		
 
-																			}
-																			else if (z > 9)
-																			{
-																			_putchar(',');
-																			_putchar(32);
-																			_putchar(32);
-																			_putchar(((z / 10) % 10) + '0');
-																			_putchar((z % 10) + '0');
-																			}
-																			else
-																			{
-																			if (y != 0)
-																			{
-																			_putchar(',');
-																			_putchar(32);
-																			_putchar(32);
-																			_putchar(32);
-																			}
-																			_putchar(z + '0');
-																			}
-																			}
+}
 _putchar('\n');
-
-
 }
 
 }
