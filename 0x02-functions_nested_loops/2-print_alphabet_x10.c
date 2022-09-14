653539e51e58cffe36ec602e8df98a56be7b0f18
BabaDee94 <adedayoadenle27@gmail.com>
2022-09-13 18:54:33 -0700

    love tech

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - function to print abc 10 times   
+ * print_alphabet_x10- function to print abc 10 times   
  * Return: 0
  */
 void print_alphabet_x10(void)
@@ -10,9 +10,8 @@ char c, i;
 for (i = 0; i <= 9; i++)
 {
 for (c = 'a'; c <= 'z'; c++)
-										{
-										_putchar(c);
-										}
-										_putchar('\n');
-										}
+							
+_putchar(c);
+}
+_putchar('\n');
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_sign - function to check for a sign of a number
+ * print_sign- function to check for a sign of a number
  * @n:  is the int that will use for the argument of the function
  * Return: 0
  */
@@ -19,6 +19,6 @@ return (-1);
 else
 {
 _putchar('0');
-return (0)
+oreturn ('0');
 }
 }
