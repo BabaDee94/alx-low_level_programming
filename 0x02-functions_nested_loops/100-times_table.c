4368d5eba43dd1195e24767d2d7bb202e0b1c34f
BabaDee94 <adedayoadenle27@gmail.com>
2022-09-14 01:10:23 -0700

    love tech

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,6 @@
 
 
 /**
- *
  * print_times_table - Prints a multiplication table up to param
  * @n: The number to be treated
  * Return: Number matrix
@@ -13,10 +12,7 @@ void print_times_table(int n)
 
 int x, y, z;
 
-
-
 if (n >= 0 && n <= 14)
-
 {
 
 for (x = 0; x <= n; x++)
@@ -25,29 +21,22 @@ for (x = 0; x <= n; x++)
 
 for (y = 0; y <= n; y++)
 
-																			{
+{
 																			z = x * y;
 																			if (z > 99)
 																			{
-
 																			_putchar(',');
-
-
 																			_putchar(32);
-
 																			_putchar((z / 100) + '0');
-																			_putchar(((z / 10) % 10) + '0');																																																				
-_putchar((z % 10) + '0');
+																			_putchar(((z / 10) % 10) + '0');																																	
+																			_putchar((z % 10) + '0');
 
 																			}
 																			else if (z > 9)
 																			{
 																			_putchar(',');
-
 																			_putchar(32);
-
 																			_putchar(32);
-
 																			_putchar(((z / 10) % 10) + '0');
 																			_putchar((z % 10) + '0');
 																			}
@@ -64,6 +53,8 @@ _putchar((z % 10) + '0');
 																			}
 																			}
 _putchar('\n');
+
+
 }
 
 }
